ed163a9c6673468d47f5a3725adb6ce0028f96f3
Mattijs Ugen <mattijsugen@gmail.com>
2012-09-07 20:57:22 +0200

    Revamp config, use simple class structure
    Using classes allows referring to values using a dot notation, more
    friendly than dictionary indexing and clearer in code.
    
    Thanks to @Dutchy- for the first move on this ;)

--- lobotomy/config.py
+++ lobotomy/config.py
@@ -1,8 +1,21 @@
+# make sure flake8 ignores this file: flake8: noqa
 
-game = dict()
-game['max_players'] = 10
-game['port'] = 1452
-game['host'] = "0.0.0.0"
+# store network details
+class host:
+	# network address to bind to
+	address = ''
+	# port to listen on (1452)
+	port = sum(map(ord, 'LoBotomyServer'))
 
-robot = dict()
-robot['energy'] = 100
+# store general game settings
+class game:
+	# turn length, time in ms the server will wait before executing a turn
+	turn_duration = 5000
+
+# store player settings
+class player:
+	# maximum and starting energy for players
+	max_energy = 1.0
+	# amount of energy that is recharged at the end of each turn
+	turn_charge = 0.2
+
