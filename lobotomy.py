b7a3a95172486bcf2c529fc9e17f081e79a369eb
Mattijs Ugen <mattijsugen@gmail.com>
2012-09-07 22:40:38 +0200

    Fix lobotomy.py
    Now @Dutchy- will have a working setup again, right? :D

--- lobotomy.py
+++ lobotomy.py
@@ -1,19 +1,25 @@
 import signal
 import sys
+
 from lobotomy import config
 from lobotomy.server import LoBotomyServer
 
+# create a server object
+server = LoBotomyServer(config.host.address, config.host.port)
 
-# Create a server object
-server = LoBotomyServer(config.game['host'], config.game['port'])
-
-# Define a shutdown handler
+# define a shutdown handler
 def shutdown(signal, frame):
 	print("Shutting down...")
 	server.shutdown()
 	#sys.exit(0)
 
-# Add a signal before serving
+# add a signal before serving
 signal.signal(signal.SIGINT, shutdown)
 
+# setup simple logging to print messages from server
+import logging
+logging.basicConfig(level = 'DEBUG')
+
+# start the server
 server.serve_forever()
+
