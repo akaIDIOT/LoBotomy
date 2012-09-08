eb0489d9625470dae7643a53e623734f01966bfa
Mattijs Ugen <mattijsugen@gmail.com>
2012-09-08 13:51:12 +0200

    Provide distance utility

--- lobotomy/util.py
+++ lobotomy/util.py
@@ -1,7 +1,22 @@
 # global utilities
+
+import math
+
 def enum(*values):
 	"""
 	Creates an enumeration with integer values.
 	"""
 	enums = dict(zip(values, range(len(values))))
 	return type('Enum', (), enums)
+
+def distance(a, b):
+	"""
+	Calculates the distance between two points encoded as (x, y).
+	"""
+	ax, ay = a
+	bx, by = b
+
+	dx = ax - bx
+	dy = ay - by
+
+	return math.sqrt(dx ** 2 + dy ** 2)
