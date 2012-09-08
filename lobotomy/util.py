0362642f0c3a402b7ae99726b77a3113a501e54a
Mattijs Ugen <mattijsugen@gmail.com>
2012-09-08 14:12:52 +0200

    Provide angle utility

--- lobotomy/util.py
+++ lobotomy/util.py
@@ -9,6 +9,19 @@ def enum(*values):
 	enums = dict(zip(values, range(len(values))))
 	return type('Enum', (), enums)
 
+def angle(a, b):
+	"""
+	Calculates the angle between point two points encoded as (x, y), as seen
+	from the first.
+	"""
+	ax, ay = a
+	bx, by = b
+
+	dx = bx - ax
+	dy = by - ay
+
+	return math.atan2(dx, dy) % (2 * math.pi)
+
 def distance(a, b):
 	"""
 	Calculates the distance between two points encoded as (x, y).
