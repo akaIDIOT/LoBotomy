b3071a67342a25e97bea6c7c42299dca9f9d6947
Mattijs Ugen <mattijsugen@gmail.com>
2012-09-09 19:52:25 +0200

    Stub unittests

---
+++ tests/test_quadtree.py
@@ -0,0 +1,27 @@
+import unittest
+
+class TestRegion(unittest.TestCase):
+	# TODO: test constructor
+	# TODO: test is_leaf
+	# TODO: test containment
+
+	# TODO: test regular insertion
+	# TODO: test insertion causing split
+	# TODO: test regular removal
+	# TODO: test removal causing merge
+
+	# TODO: test overlaps
+	pass
+
+class TestQuadTree(unittest.TestCase):
+	# TODO: test constructor
+	# TODO: test find_all
+	pass
+
+class TestMove(unitttest.TestCase):
+	# TODO: test moving point to the same region
+	# TODO: test moving point to close region
+	# TODO: test moving point to region requiring traversing through root
+	# TODO: test moving point outside of tree
+	pass
+
