fde83d0bf533492e0b978a70c7090f42a94f48e5
Mattijs Ugen <mattijsugen@gmail.com>
2012-09-20 22:34:31 +0200

    Stub unit tests for lobotomy.util
    References #7

---
+++ tests/test_util.py
@@ -0,0 +1,13 @@
+import unittest
+
+from lobotomy import util
+
+class TestAngle(unittest.TestCase):
+	pass
+
+class TestDistance(unittest.TestCase):
+	pass
+
+class TestWrappedBoundsGenerator(unittest.TestCase):
+	pass
+
