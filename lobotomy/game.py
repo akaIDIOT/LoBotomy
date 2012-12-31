2f8973d1ee94eda9e6797c4b8b2de2e30b197069
Peter Wagenaar <sakartu@gmail.com>
2012-12-31 14:03:42 +0100

    Add inverse cost functions (and some docs)

--- lobotomy/game.py
+++ lobotomy/game.py
@@ -5,11 +5,32 @@
 import math
 
 def move_cost(distance):
-	return distance * 2
+    '''
+    Returns the cost (in energy) of moving the given distance
+    '''
+	return distance * 2.0
+
+def move_cost_inverse(cost):
+    '''
+    Returns the distance one could move with the given energy cost
+    '''
+    return cost / 2.0
 
 def scan_cost(radius):
-	return (radius * 2) ** 2
+    '''
+    Returns the cost (in energy) of scanning the given radius
+    '''
+	return (radius * 2.0) ** 2.0
+
+def scan_cost_inverse(cost):
+    '''
+    Returns the radius one could scan with the given energy cost
+    '''
+    return math.sqrt(cost) / 2.0
 
 def fire_cost(distance, radius, charge):
-	return distance + math.pi * radius ** 2 * charge
-
+    '''
+    Returns the cost (in energy) of firing the given distance, with the given
+    radius and charge
+    '''
+    return distance + math.pi * radius ** 2.0 * charge
