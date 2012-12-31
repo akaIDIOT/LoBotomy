513bf09ce4dd45ba6877874f762876b4e1d633d1
Peter Wagenaar <sakartu@gmail.com>
2012-12-31 14:47:18 +0100

    Change ffing spaces to ffing tabs (stupid convention differences...)

--- lobotomy/game.py
+++ lobotomy/game.py
@@ -5,32 +5,32 @@
 import math
 
 def move_cost(distance):
-    '''
-    Returns the cost (in energy) of moving the given distance
-    '''
+	'''
+	Returns the cost (in energy) of moving the given distance
+	'''
 	return distance * 2.0
 
 def move_cost_inverse(cost):
-    '''
-    Returns the distance one could move with the given energy cost
-    '''
-    return cost / 2.0
+	'''
+	Returns the distance one could move with the given energy cost
+	'''
+	return cost / 2.0
 
 def scan_cost(radius):
-    '''
-    Returns the cost (in energy) of scanning the given radius
-    '''
+	'''
+	Returns the cost (in energy) of scanning the given radius
+	'''
 	return (radius * 2.0) ** 2.0
 
 def scan_cost_inverse(cost):
-    '''
-    Returns the radius one could scan with the given energy cost
-    '''
-    return math.sqrt(cost) / 2.0
+	'''
+	Returns the radius one could scan with the given energy cost
+	'''
+	return math.sqrt(cost) / 2.0
 
 def fire_cost(distance, radius, charge):
-    '''
-    Returns the cost (in energy) of firing the given distance, with the given
-    radius and charge
-    '''
-    return distance + math.pi * radius ** 2.0 * charge
+	'''
+	Returns the cost (in energy) of firing the given distance, with the given
+	radius and charge
+	'''
+	return distance + math.pi * radius ** 2.0 * charge
